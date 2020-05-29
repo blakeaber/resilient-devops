86e23d1858d7cc84313cb17904c4480398818c18
Marc Ports AI <mutayoba.air@gmail.com>
2020-05-29 03:08:38 -0400

    Update main.tf

--- tfs/docker_eb/main.tf
+++ tfs/docker_eb/main.tf
@@ -54,6 +54,7 @@ resource "aws_iam_role_policy" "ng_beanstalk_ec2_policy" {
     {
       "Action": [
         "cloudwatch:PutMetricData",
+        "elasticbeanstalk:*",
         "ds:CreateComputer",
         "ds:DescribeDirectories",
         "ec2:DescribeInstanceStatus",
