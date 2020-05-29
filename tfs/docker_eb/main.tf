dda7a7e723c186db53a8f1f5b397ea8a6d5ec542
Marc Ports AI <mutayoba.air@gmail.com>
2020-05-28 22:07:43 -0400

    Update main.tf

--- tfs/docker_eb/main.tf
+++ tfs/docker_eb/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 # S3 Bucket for storing Elastic Beanstalk task definitions
 resource "aws_s3_bucket" "ng_beanstalk_deploys" {
   bucket = "${var.application_name}-deployments"
+  region = "${var.region}"
 }
 
 # Elastic Container Repository for Docker images
