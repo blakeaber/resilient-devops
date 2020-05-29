841489620433cc4af006258b0075547a0f34d35d
Marc Ports AI <mutayoba.air@gmail.com>
2020-05-29 19:38:31 -0400

    bug fix investigation causing crash

--- tfs/docker_eb/main.tf
+++ tfs/docker_eb/main.tf
@@ -248,28 +248,28 @@ data "aws_route53_zone" "primary" {
   # depends_on = [ "aws_elastic_beanstalk_environment.ng_beanstalk_application_environment" ]
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "${data.aws_route53_zone.primary.zone_id}"
-  name    = "www.${var.site}"
-  type    = "A"
+# resource "aws_route53_record" "www" {
+#   zone_id = "${data.aws_route53_zone.primary.zone_id}"
+#   name    = "www.${var.site}"
+#   type    = "A"
 
-  alias {
-    name                   = "${data.aws_lb.alb.dns_name}"
-    zone_id                = "${data.aws_lb.alb.zone_id}"
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = "${data.aws_lb.alb.dns_name}"
+#     zone_id                = "${data.aws_lb.alb.zone_id}"
+#     evaluate_target_health = true
+#   }
   
-}
+# }
 
-resource "aws_route53_record" "none_www" {
-  zone_id = "${data.aws_route53_zone.primary.zone_id}"
-  name    = "${var.site}"
-  type    = "A"
+# resource "aws_route53_record" "none_www" {
+#   zone_id = "${data.aws_route53_zone.primary.zone_id}"
+#   name    = "${var.site}"
+#   type    = "A"
 
-  alias {
-    name                   = "${data.aws_lb.alb.dns_name}"
-    zone_id                = "${data.aws_lb.alb.zone_id}"
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = "${data.aws_lb.alb.dns_name}"
+#     zone_id                = "${data.aws_lb.alb.zone_id}"
+#     evaluate_target_health = true
+#   }
   
 }
